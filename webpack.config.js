3ceb388df0cc8385ce7a2981b14b9d408238c05c
Filippo Sarzana <filippo@sarzana.me>
2017-12-15 09:54:17 +0100

    chore(): fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,24 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['*', '.js'],
+    libraryTarget: 'commonjs2',
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    }],
+    rules: [
+      {
+        test: /\.(js|png)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(build|setupTests|node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   },
+  externals: {
+    'react': 'commonjs react',
+  }
 }
