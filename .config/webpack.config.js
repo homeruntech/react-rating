3004559eab00f5c196bdab9790e183d131321439
Filippo Sarzana <filippo@sarzana.me>
2017-12-20 18:57:02 +0100

    feat(): minimal style for github pages

--- .config/webpack.config.js
+++ .config/webpack.config.js
@@ -1,21 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path')
-const webpack = require('webpack') // eslint-disable-line import/no-extraneous-dependencies
-const HtmlWebpackPlugin = require('html-webpack-plugin') // eslint-disable-line import/no-extraneous-dependencies
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
 
-const EXAMPLES_DIR = path.join(__dirname, '..', '.examples')
+const DOCS_DIR = path.join(__dirname, '..', 'docs')
 const SOURCE_DIR = path.join(__dirname, '..', 'src')
+const TEMPLATES_DIR = path.join(__dirname, '..', '.templates')
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: path.join(EXAMPLES_DIR, 'index.html'),
+  template: path.join(TEMPLATES_DIR, 'index.html'),
   filename: 'index.html',
   inject: 'body',
 })
 
 module.exports = {
-  entry: path.join(EXAMPLES_DIR, 'index.js'),
+  devServer: {
+    inline: true,
+  },
+  entry: path.join(TEMPLATES_DIR, 'index.js'),
   output: {
     filename: '[name].js',
-    path: path.join(EXAMPLES_DIR),
+    path: path.join(DOCS_DIR),
     publicPath: '/',
   },
   module: {
@@ -36,5 +42,6 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin('shared'),
     new webpack.LoaderOptionsPlugin({ debug: true }),
     HtmlWebpackPluginConfig,
+    new OpenBrowserPlugin(),
   ],
 }
